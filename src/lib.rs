e0c8a62854ddbfaf2c006b98f30f69f836a29159
Max Prettyjohns <heshoots9999@gmail.com>
2019-06-15 00:46:21 +0100

    project 1.6

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,41 @@
-use std::string::*;
-use std::option::*;
 use std::collections::HashMap;
+use std::option::*;
+use std::string::*;
 
 #[derive(Default)]
 pub struct KvStore {
-    store : HashMap<String, String>
+    store: HashMap<String, String>,
 }
 
 impl KvStore {
     pub fn new() -> KvStore {
-        KvStore { store : HashMap::new() }
+        KvStore {
+            store: HashMap::new(),
+        }
     }
 
-    pub fn set(&mut self, a : String, b : String) {
+    /// Sets a value in store
+    pub fn set(&mut self, a: String, b: String) {
         self.store.insert(a, b);
     }
 
-    pub fn get(&mut self, a : String) -> Option<String> {
+    /// Get a value from a store
+    ///
+    /// Returns None where value doesn't exist
+    pub fn get(&mut self, a: String) -> Option<String> {
         if self.store.contains_key(&a) {
             let value = self.store.get(&a);
             match value {
-                Some(x) => { return Some(x.to_string()) }
-                _ => { return None }
+                Some(x) => return Some(x.to_string()),
+                _ => return None,
             }
         }
         None
     }
 
-    pub fn remove(&mut self, a : String) {
+    /// Remove a value from the store
+    /// Always succeeds regardless of existence in the store
+    pub fn remove(&mut self, a: String) {
         self.store.remove_entry(&a);
     }
 }
